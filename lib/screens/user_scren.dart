75f362dee1d27d6e7523bc4abbe4dba83bce48a8
nuaiman <ashiqnuaiman@gmail.com>
2021-12-06 12:29:53 +0600

    commit

--- lib/screens/user_scren.dart
+++ lib/screens/user_scren.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class UserScreen extends StatefulWidget {
@@ -10,116 +12,177 @@ class UserScreen extends StatefulWidget {
 }
 
 class _UserScreenState extends State<UserScreen> {
+  double top = 0;
+
+  late ScrollController _scrollController;
+
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+    _scrollController.addListener(() {
+      setState(() {});
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            pinned: true,
-            stretch: true,
-            expandedHeight: 250,
-            flexibleSpace: FlexibleSpaceBar(
-              centerTitle: true,
-              background: Image.network(
-                'https://images.pexels.com/photos/1537875/pexels-photo-1537875.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
-                fit: BoxFit.cover,
-              ),
-              title: Row(
-                children: const [
-                  SizedBox(
-                    width: 12,
-                  ),
-                  CircleAvatar(
-                    backgroundImage: NetworkImage(
-                        'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
-                  ),
-                  SizedBox(
-                    width: 12,
-                  ),
-                  Text('Fluttercraft')
-                ],
-              ),
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: ListView(
-                primary: false,
-                shrinkWrap: true,
-                children: [
-                  // User Bag
-                  const _userTileText(text: 'User Bag'),
-                  const _userTileHeightSpace(height: 10),
-                  _userListTile(
-                    onTap: () {},
-                    lIcon: Icons.favorite,
-                    color: Colors.red,
-                    title: 'Wishlist',
-                    tIcon: Icons.arrow_forward_ios,
-                    tIconCallBack: () {},
-                  ),
-                  _userListTile(
-                    onTap: () {},
-                    lIcon: Icons.shopping_cart,
-                    color: Colors.deepPurpleAccent,
-                    title: 'Cart',
-                    tIcon: Icons.arrow_forward_ios,
-                    tIconCallBack: () {},
-                  ),
-
-                  // User Settings
-                  const _userTileHeightSpace(height: 15),
-                  const _userTileText(text: 'User Settings'),
-                  const _userTileHeightSpace(height: 10),
-                  Card(
-                    child: SwitchListTile.adaptive(
-                      secondary:
-                          Icon(Icons.light_mode, color: Colors.amber.shade700),
-                      title: const Text('Light Mode'),
-                      value: false,
-                      onChanged: (value) {},
+      body: Stack(
+        children: [
+          CustomScrollView(
+            controller: _scrollController,
+            slivers: [
+              SliverAppBar(
+                pinned: true,
+                stretch: true,
+                expandedHeight: 250,
+                flexibleSpace: LayoutBuilder(builder: (ctx, cons) {
+                  top = cons.biggest.height;
+                  return FlexibleSpaceBar(
+                    centerTitle: true,
+                    background: Image.network(
+                      'https://images.pexels.com/photos/1537875/pexels-photo-1537875.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+                      fit: BoxFit.cover,
                     ),
-                  ),
-
-                  // User Information
-                  const _userTileHeightSpace(height: 15),
-                  const _userTileText(text: 'User Informattion'),
-                  const _userTileHeightSpace(height: 10),
-
-                  _userListTile(
-                    lIcon: Icons.email,
-                    color: Colors.green,
-                    title: 'Email',
-                    subTitle: 'Email',
-                    onTap: () {},
-                  ),
-                  _userListTile(
-                    lIcon: Icons.call,
-                    color: Colors.yellow.shade800,
-                    title: 'Phone Number',
-                    subTitle: 'Number',
-                    onTap: () {},
-                  ),
-                  _userListTile(
-                    lIcon: Icons.local_shipping,
-                    color: Colors.indigo,
-                    title: 'Address',
-                    subTitle: 'Address',
-                    onTap: () {},
-                  ),
-                  const _userListTile(
-                    lIcon: Icons.watch_later,
-                    color: Colors.redAccent,
-                    title: 'Join Date',
-                    subTitle: 'date',
-                  ),
-                ],
+                    title: AnimatedOpacity(
+                      duration: const Duration(milliseconds: 300),
+                      opacity: top <= 200 ? 1.0 : 0.0,
+                      child: Row(
+                        children: const [
+                          SizedBox(
+                            width: 12,
+                          ),
+                          CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+                          ),
+                          SizedBox(
+                            width: 12,
+                          ),
+                          Text('Fluttercraft')
+                        ],
+                      ),
+                    ),
+                  );
+                }),
               ),
-            ),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: ListView(
+                    primary: false,
+                    shrinkWrap: true,
+                    children: [
+                      // User Bag
+                      const _userTileText(text: 'User Bag'),
+                      const _userTileHeightSpace(height: 10),
+                      _userListTile(
+                        onTap: () {},
+                        lIcon: Icons.favorite,
+                        color: Colors.red,
+                        title: 'Wishlist',
+                        tIcon: Icons.arrow_forward_ios,
+                        tIconCallBack: () {},
+                      ),
+                      _userListTile(
+                        onTap: () {},
+                        lIcon: Icons.shopping_cart,
+                        color: Colors.deepPurpleAccent,
+                        title: 'Cart',
+                        tIcon: Icons.arrow_forward_ios,
+                        tIconCallBack: () {},
+                      ),
+
+                      // User Settings
+                      const _userTileHeightSpace(height: 15),
+                      const _userTileText(text: 'User Settings'),
+                      const _userTileHeightSpace(height: 10),
+                      Card(
+                        child: SwitchListTile.adaptive(
+                          secondary: Icon(Icons.light_mode,
+                              color: Colors.amber.shade700),
+                          title: const Text('Light Mode'),
+                          value: false,
+                          onChanged: (value) {},
+                        ),
+                      ),
+
+                      // User Information
+                      const _userTileHeightSpace(height: 15),
+                      const _userTileText(text: 'User Informattion'),
+                      const _userTileHeightSpace(height: 10),
+                      _userListTile(
+                        lIcon: Icons.call,
+                        color: Colors.green.shade700,
+                        title: 'Phone Number',
+                        subTitle: 'Number',
+                        onTap: () {},
+                      ),
+                      _userListTile(
+                        lIcon: Icons.email,
+                        color: Colors.yellow.shade700,
+                        title: 'Email',
+                        subTitle: 'Email',
+                        onTap: () {},
+                      ),
+
+                      _userListTile(
+                        lIcon: Icons.local_shipping,
+                        color: Colors.indigo.shade400,
+                        title: 'Address',
+                        subTitle: 'Address',
+                        onTap: () {},
+                      ),
+                      _userListTile(
+                        lIcon: Icons.watch_later,
+                        color: Colors.redAccent.shade200,
+                        title: 'Join Date',
+                        subTitle: 'date',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
+          _buildFab(),
         ],
+      ),
+    );
+  }
+
+  Widget _buildFab() {
+    double defaultMargin = Platform.isIOS ? 270 : 250;
+    double scrollStart = 230;
+    double scrollEnd = scrollStart / 2;
+
+    double top = defaultMargin;
+    double scale = 1.0;
+
+    if (_scrollController.hasClients) {
+      double offSet = _scrollController.offset;
+      top -= offSet;
+
+      if (offSet < defaultMargin - scrollStart) {
+        scale = 1;
+      } else if (offSet < defaultMargin - scrollEnd) {
+        scale = (defaultMargin - scrollEnd - offSet) / scrollEnd;
+      } else {
+        scale = 0;
+      }
+    }
+
+    return Positioned(
+      top: top,
+      right: 20,
+      child: Transform(
+        transform: Matrix4.identity()..scale(scale),
+        alignment: Alignment.center,
+        child: FloatingActionButton(
+          child: const Icon(Icons.camera),
+          onPressed: () {},
+        ),
       ),
     );
   }
