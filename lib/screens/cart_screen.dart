3fe7adf4a27a4112600a9672ecc24f6e8629ed43
nuaiman <ashiqnuaiman@gmail.com>
2021-12-10 16:57:47 +0600

    commit

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -10,8 +10,62 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List _products = [];
-    return Scaffold(
-      body: _products.isEmpty ? EmptyCart() : FullCart(),
-    );
+    return !_products.isEmpty
+        ? Scaffold(
+            body: EmptyCart(),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              title: Text('Cart'),
+              actions: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.delete),
+                ),
+              ],
+            ),
+            body: Container(
+              margin: EdgeInsets.only(bottom: 60),
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (ctx, i) {
+                  return FullCart();
+                },
+              ),
+            ),
+            bottomSheet: _bottomCheckoutSectiomn(),
+          );
   }
+}
+
+Widget _bottomCheckoutSectiomn() {
+  return SizedBox(
+    width: double.infinity,
+    child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Text(
+              'Total: \$ 450.00',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              '   C H E C K O U T   ',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
 }
--- lib/widgets/full_cart.dart
+++ lib/widgets/full_cart.dart
@@ -10,139 +10,132 @@ class FullCart extends StatefulWidget {
 class _FullCartState extends State<FullCart> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Cart'),
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8),
-          child: Container(
-            width: double.infinity,
-            height: 150,
-            decoration: BoxDecoration(
-              border: Border.all(width: 2, color: Colors.grey),
-              borderRadius: const BorderRadius.only(
-                topRight: Radius.circular(16),
-                bottomRight: Radius.circular(16),
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Container(
+        width: double.infinity,
+        height: 150,
+        decoration: BoxDecoration(
+          border: Border.all(width: 2, color: Colors.grey),
+          borderRadius: const BorderRadius.only(
+            topRight: Radius.circular(16),
+            bottomRight: Radius.circular(16),
+          ),
+        ),
+        child: Row(
+          children: [
+            Container(
+              width: 130,
+              decoration: const BoxDecoration(
+                color: Colors.grey,
+                image: DecorationImage(
+                  image: NetworkImage(
+                    'https://images.pexels.com/photos/4600998/pexels-photo-4600998.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+                  ),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-            child: Row(
-              children: [
-                Container(
-                  width: 130,
-                  decoration: const BoxDecoration(
-                    color: Colors.grey,
-                    image: DecorationImage(
-                      image: NetworkImage(
-                        'https://images.pexels.com/photos/4600998/pexels-photo-4600998.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-                      ),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 10),
-                Flexible(
-                  child: Column(
+            const SizedBox(width: 10),
+            Flexible(
+              child: Column(
+                children: [
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const SizedBox(height: 10),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Flexible(
-                            child: Text(
-                              'Monitor 2',
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(fontSize: 18),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(right: 8.0),
-                            child: InkWell(
-                              onTap: () {},
-                              child: const Icon(
-                                Icons.close,
-                                color: Colors.red,
-                              ),
-                            ),
-                          ),
-                        ],
+                      const Flexible(
+                        child: Text(
+                          'Monitor 2',
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(fontSize: 18),
+                        ),
                       ),
-                      const SizedBox(height: 7),
-                      Row(
-                        children: [
-                          const Text(
-                            'Price : ',
-                            style: TextStyle(fontSize: 16),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 8.0),
+                        child: InkWell(
+                          onTap: () {},
+                          child: const Icon(
+                            Icons.close,
+                            color: Colors.red,
                           ),
-                          Flexible(
-                            child: Text(
-                              '\$450.00',
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(fontSize: 16),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          const Text(
-                            'SubTotal : ',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                          Flexible(
-                            child: Text(
-                              '\$450.00',
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(fontSize: 16),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          const Text(
-                            'Shipping : ',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                          Flexible(
-                            child: Text(
-                              '\$450.00',
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(fontSize: 16),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          TextButton(
-                            onPressed: () {},
-                            child: const Text(
-                              '-',
-                              style: TextStyle(fontSize: 25),
-                            ),
-                          ),
-                          Text(
-                            '1',
-                            style: TextStyle(fontSize: 20),
-                          ),
-                          TextButton(
-                            onPressed: () {},
-                            child: const Text(
-                              '+',
-                              style: TextStyle(fontSize: 20),
-                            ),
-                          ),
-                        ],
+                        ),
                       ),
                     ],
                   ),
-                ),
-              ],
+                  const SizedBox(height: 7),
+                  Row(
+                    children: [
+                      const Text(
+                        'Price : ',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      Flexible(
+                        child: Text(
+                          '\$450.00',
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Text(
+                        'SubTotal : ',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      Flexible(
+                        child: Text(
+                          '\$450.00',
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Text(
+                        'Shipping : ',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      Flexible(
+                        child: Text(
+                          '\$450.00',
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          '-',
+                          style: TextStyle(fontSize: 25),
+                        ),
+                      ),
+                      Text(
+                        '1',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          '+',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
