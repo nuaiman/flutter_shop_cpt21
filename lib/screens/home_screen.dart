f58c7267becb0a10e606e86c840b84028b8b723d
nuaiman <ashiqnuaiman@gmail.com>
2021-12-11 08:15:50 +0600

    commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,7 @@
+import 'package:backdrop/app_bar.dart';
+import 'package:backdrop/button.dart';
+import 'package:backdrop/scaffold.dart';
+import 'package:backdrop/sub_header.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,8 +12,33 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
+      body: BackdropScaffold(
+        appBar: BackdropAppBar(
+          title: Text('Flutter Shop'),
+          leading: BackdropToggleButton(
+            icon: AnimatedIcons.home_menu,
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: CircleAvatar(
+                  radius: 14,
+                  backgroundImage: NetworkImage(
+                    'https://cdn1.vectorstock.com/i/thumb-large/62/60/default-avatar-photo-placeholder-profile-image-vector-21666260.jpg',
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        backLayer: Center(
+          child: Text("Back Layer"),
+        ),
+        frontLayer: Center(
+          child: Text("Front Layer"),
+        ),
       ),
     );
   }
