967c52132d0ab249c5391d6a8350be2f58eb6120
nuaiman <ashiqnuaiman@gmail.com>
2021-12-22 14:18:51 +0600

    commit

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -2,31 +2,141 @@ import 'package:flutter/material.dart';
 import 'package:wave/config.dart';
 import 'package:wave/wave.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  void _chanageVisibility() {
+    setState(() {
+      _isVisible = !_isVisible;
+    });
+  }
+
+  final _formKey = GlobalKey<FormState>();
+
+  String _email = '';
+  String _password = '';
+
+  bool _isVisible = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          RotatedBox(
-            quarterTurns: 2,
-            child: WaveWidget(
-              config: CustomConfig(
-                colors: [
-                  Colors.orangeAccent,
-                  Colors.tealAccent,
-                  Colors.pinkAccent,
-                  Colors.deepPurpleAccent,
-                ],
-                durations: [35000, 11000, 10800, 6000],
-                heightPercentages: [0.01, 0.02, 0.03, 0.1],
-                blur: const MaskFilter.blur(BlurStyle.solid, 0),
-              ),
-              heightPercentange: 0.2,
-              size: const Size(double.infinity, double.infinity),
+          // RotatedBox(
+          //   quarterTurns: 2,
+          //   child: WaveWidget(
+          //     config: CustomConfig(
+          //       colors: [
+          //         Colors.orangeAccent,
+          //         Colors.tealAccent,
+          //         Colors.pinkAccent,
+          //         Colors.deepPurpleAccent,
+          //       ],
+          //       durations: [35000, 11000, 10800, 6000],
+          //       heightPercentages: [0.01, 0.02, 0.03, 0.1],
+          //       blur: const MaskFilter.blur(BlurStyle.solid, 0),
+          //     ),
+          //     heightPercentange: 0.2,
+          //     size: const Size(double.infinity, double.infinity),
+          //   ),
+          // ),
+
+          Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const SizedBox(height: 80),
+                Center(
+                  child: Text(
+                    'Login',
+                    style: TextStyle(fontSize: 65),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                TextFormField(
+                  onSaved: (value) {
+                    _email = value!;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  key: ValueKey('email'),
+                  validator: (value) {
+                    if (value!.isEmpty || !value.contains('@')) {
+                      return 'Please enter a valid email address';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    prefixIcon: Icon(Icons.email),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  onSaved: (value) {
+                    _password = value!;
+                  },
+                  obscureText: _isVisible,
+                  key: ValueKey('password'),
+                  validator: (value) {
+                    if (value!.isEmpty || value.length < 8) {
+                      return 'Password must be atleast 8 units';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    prefixIcon: Icon(Icons.lock),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _isVisible = !_isVisible;
+                        });
+                      },
+                      icon: Icon(
+                          _isVisible ? Icons.visibility : Icons.visibility_off),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Create an Account',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
             ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Center(
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Create an Account',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+            ],
           ),
         ],
       ),
